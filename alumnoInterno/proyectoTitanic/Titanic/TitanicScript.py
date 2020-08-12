409370ab4c2c307a65595457bb762e97f722c03d
felipesca99 <felipescalera99@gmail.com>
2020-08-13 00:36:01 +0200

    Trabajo Titanic (lectura y comprobaciones básicas)

--- alumnoInterno/proyectoTitanic/Titanic/TitanicScript.py
+++ alumnoInterno/proyectoTitanic/Titanic/TitanicScript.py
@@ -5,4 +5,18 @@ Created on Mon Feb 24 19:46:26 2020
 @author: felip
 """
 
+""" Importamos todo lo necesario para comenzar """
+
 import pandas as pd
+import numpy as np
+import random as rnd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+training = pd.read_csv('../dataTitanic/train.csv')
+testing = pd.read_csv('../dataTitanic/test.csv')
+total = [training, testing]
+
+
+total.head()
---
+++ alumnoInterno/proyectoTitanic/Titanic/funciones.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 00:13:09 2020
+
+@author: felip
+"""
+
