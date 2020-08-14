73f98fe3a9591c833495cda0d6cddf87cd7a50dc
felipesca99 <felipescalera99@gmail.com>
2020-08-14 19:58:30 +0200

    Creación de los datasets y estudio básico

--- alumnoInterno/proyectoTitanic/Titanic/TitanicScript.py
+++ alumnoInterno/proyectoTitanic/Titanic/TitanicScript.py
@@ -10,13 +10,23 @@ Created on Mon Feb 24 19:46:26 2020
 import pandas as pd
 import numpy as np
 import random as rnd
+import funciones as fn
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
-training = pd.read_csv('../dataTitanic/train.csv')
-testing = pd.read_csv('../dataTitanic/test.csv')
+training = pd.read_csv('dataTitanic/train.csv')
+testing = pd.read_csv('dataTitanic/test.csv')
 total = [training, testing]
 
+"""total_df = pd.DataFrame(total) -> definir pd dataset a través de lista"""
 
-total.head()
+print("Cola y cabeza del entrenamiento")
+fn.tails_and_heads(training)
+print("Total de casos: "+str(len(training))+"\n\n")
+
+print("Cola y cabeza del testeo")
+fn.tails_and_heads(testing)
+print("Total de casos: "+str(len(testing))+"\n\n")
+
+print(training.describe())
--- alumnoInterno/proyectoTitanic/Titanic/funciones.py
+++ alumnoInterno/proyectoTitanic/Titanic/funciones.py
@@ -5,3 +5,6 @@ Created on Thu Aug 13 00:13:09 2020
 @author: felip
 """
 
+def tails_and_heads(dataset):
+    print(dataset.head())
+    print(dataset.tail())
